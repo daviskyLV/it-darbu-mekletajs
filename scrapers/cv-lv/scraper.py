b93361c4d6cc156c54c9e60f8c26eb3c1dfb2ecd
Andrejs Dāvis Umbrovskis <ledaviskylv@gmail.com>
2025-06-24 01:39:06 +0300

    Updated cv.lv scraper to work with new database

--- scrapers/cv-lv/scraper.py
+++ scrapers/cv-lv/scraper.py
@@ -9,9 +9,9 @@ from utils.parser import parse_image_file_to_string, remove_html_tags
 
 DOMAIN: str = "cv.lv"
 
-def get_vacancies_list(keywords_json: dict[str, dict[str, list[str]]]) -> list[Vacancy]:
+def get_vacancies_list() -> list[str]:
     """
-    Returns a list of all available vacancies with partially filled info
+    Returns a list of all available vacancies (only their ids)
     """
     # simple get request of 10000 listings is enough to get all of them
     vacancies_req = requests.get("https://cv.lv/api/v1/vacancy-search-service/search?limit=10000&offset=0")
@@ -19,48 +19,12 @@ def get_vacancies_list(keywords_json: dict[str, dict[str, list[str]]]) -> list[V
         raise Exception(f"Couldn't get vacancies list! Error code: {vacancies_req.status_code}")
     
     vac_json = vacancies_req.json()
-    final: list[Vacancy] = []
+    final: list[str] = []
     for v in vac_json["vacancies"]:
-        summed_description: str = ""
-        summed_description += f" {v["positionTitle"]} "
-        summed_description += f" {v["positionContent"]} "
-        if v["keywords"]:
-            for keyv in v["keywords"]:
-                summed_description += f" {keyv} "
-        
-        summarized = summary.create_summarized_description(summed_description, keywords_json)
-        # if not summary.vacancy_valid(summarized):
-        #     # vacancy didnt have any matching keywords, skipping
-        #     continue
         if not 10 in v["categories"]:
             # vacancy doesnt have "INFORMATION_TECHNOLOGY" tag, skipping
             continue
-
-        s_min: float = 0
-        s_max: float = 0
-        if v["salaryFrom"]:
-            s_min = float(v["salaryFrom"])
-        if v["salaryTo"]:
-            s_max = float(v["salaryTo"])
-        else:
-            s_max = s_min
-        final.append(Vacancy(
-                db_id=None,
-                web_id=str(v["id"]),
-                title=str(v["positionTitle"]),
-                employer=str(v["employerName"]),
-                published=dt.datetime.fromisoformat(str(v["publishDate"])),
-                expires=dt.datetime.fromisoformat(str(v["expirationDate"])),
-                salary_min=s_min,
-                salary_max=s_max,
-                hourly_rate=bool(v["hourlySalary"]),
-                remote=bool(v["remoteWork"]),
-                country_code=None,
-                city_name=None,
-                fully_fetched=False,
-                description=None,
-                summarized_description=summarized
-            ))
+        final.append(str(v["id"]))
 
     return final
 
@@ -174,7 +138,6 @@ def get_vacancy_data(nextjs_url: str, web_id: str, db_id: int,
         expires=dt.datetime.fromisoformat(str(vac_json["settings"]["dateTo"])),
         country_code=cc,
         city_name=city,
-        fully_fetched=True,
         web_id=web_id,
         description=base_desc.strip(),
         summarized_description=summarized
@@ -187,28 +150,27 @@ if __name__ == "__main__":
         float(os.getenv("WEB_REQUEST_INTERVAL_MIN", "0.5")),
         float(os.getenv("WEB_REQUEST_INTERVAL_MAX", "1.0"))
     )
-    db_req_interval = (
-        float(os.getenv("DB_REQUEST_INTERVAL_MIN", "5.0")),
-        float(os.getenv("DB_REQUEST_INTERVAL_MIN", "10.0"))
-    )
-    connection = db.get_connection()
+    db_req_interval = float(os.getenv("DB_REQUEST_INTERVAL", "3.0"))
+    nothing_todo_interval = float(os.getenv("NOTHING_TODO_INTERVAL", "60.0"))
 
     # Reading keywords.json
     keywords_json: dict[str, dict[str, list[str]]] = {}
     with open("/keywords.json", "r") as file:
         keywords_json: dict[str, dict[str, list[str]]] = json.load(file)
 
+    db_con = None
     # main loop
     while True:
-        time.sleep(get_random(db_req_interval[0], db_req_interval[1])) # delay between requests
+        if not db_con:
+            db_con = db.get_connection()
         # updating website vacancy list if its outdated
-        website_stale = db.check_if_website_stale(connection, DOMAIN)
+        website_stale = db.check_if_website_stale(db_con, DOMAIN)
         if website_stale:
             print(f"[{dt.datetime.now().isoformat()}] Website stale, rescanning...!")
-            db.set_website_scan_status(connection, DOMAIN, True)
-            vacancy_list: list[Vacancy] = []
+            db.set_website_scan_status(db_con, DOMAIN, True)
+            vacancy_list: list[str] = []
             try:
-                vacancy_list = get_vacancies_list(keywords_json)
+                vacancy_list = get_vacancies_list()
             except Exception as e:
                 print(f"[{dt.datetime.now().isoformat()}] An exception occoured while getting vacancy list!", e)
             
@@ -222,38 +184,61 @@ if __name__ == "__main__":
 
                 # inserting new vacancies into database
                 for c in chunks:
-                    db.add_new_vacancies(connection, DOMAIN, c)
+                    db.add_unscanned_vacancies(db_con, c, DOMAIN)
             except Exception as e:
-                print(f"[{dt.datetime.now().isoformat()}] An exception occoured while adding vacancies to database!", e)
+                print(f"[{dt.datetime.now().isoformat()}] An exception occoured while adding unscanned vacancies to database!", e)
             finally:
-                db.set_website_scan_status(connection, DOMAIN, False)
+                db.set_website_scan_status(db_con, DOMAIN, False)
             print(f"[{dt.datetime.now().isoformat()}] Website rescanned!")
-        
-        # getting stale vacancies to update their info
-        stale_vacancies = db.get_stale_vacancies(connection, DOMAIN)
-        if len(stale_vacancies) == 0:
-            continue
+            time.sleep(db_req_interval) # letting database rest a little
 
+        
+        # Updating unscanned & stale vacancy info
         nextjs_url: str = ""
         try:
             nextjs_url = get_nextjs_url()
         except:
             # failed to get nextjs url
+            db.close_connection(db_con)
+            db_con = None
+            time.sleep(nothing_todo_interval) # max wait time, since this is a big error
+            continue
+
+        unscanned_vacancies = db.get_unscanned_vacancies(db_con, DOMAIN)
+        if len(unscanned_vacancies) > 0:
+            # There are unscanned vacancies to process first
+            print(f"[{dt.datetime.now().isoformat()}] Fetching info for {len(unscanned_vacancies)} unscanned vacancies...")
+            fetched: list[Vacancy] = []
+            for sv in unscanned_vacancies:
+                try:
+                    data = get_vacancy_data(nextjs_url, sv[0], sv[1], keywords_json)
+                    fetched.append(data)
+                except Exception as e:
+                    print(f"[{dt.datetime.now().isoformat()}] Failed to get vacancy data for {sv[0]}", e)
+                
+                time.sleep(get_random(web_req_interval[0], web_req_interval[1])) # delay between requests
+            print(f"[{dt.datetime.now().isoformat()}] Unscanned vacancy info fetched!")
+            db.add_new_vacancies(db_con, DOMAIN, fetched)
+            ids: list[int] = [i[1] for i in unscanned_vacancies]
+            db.delete_unscanned_vacancies(db_con, ids)
+            time.sleep(db_req_interval) # letting database rest a little
+
+
+        # getting stale vacancies to update their info
+        stale_vacancies = db.get_stale_vacancies(db_con, DOMAIN)
+        if len(stale_vacancies) == 0:
+            if len(unscanned_vacancies) == 0:
+                time.sleep(nothing_todo_interval)
+            else:
+                time.sleep(get_random(web_req_interval[0], web_req_interval[1]))
             continue
 
         # Fetching full info for stale vacancies
-        print(f"[{dt.datetime.now().isoformat()}] Fetching info for {len(stale_vacancies)} vacancies...")
+        print(f"[{dt.datetime.now().isoformat()}] Fetching info for {len(stale_vacancies)} stale vacancies...")
         fetched: list[Vacancy] = []
-        #to_delete: list[int] = []
         for sv in stale_vacancies:
             try:
                 data = get_vacancy_data(nextjs_url, sv[0], sv[1], keywords_json)
-                # if not summary.vacancy_valid(data.summarized_description):
-                #     if data.db_id:
-                #         to_delete.append(data.db_id)
-                #     continue
-
-                # All good, can update in database
                 fetched.append(data)
             except Exception as e:
                 print(f"[{dt.datetime.now().isoformat()}] Failed to get vacancy data for {sv[0]}", e)
@@ -261,5 +246,6 @@ if __name__ == "__main__":
             time.sleep(get_random(web_req_interval[0], web_req_interval[1])) # delay between requests
         print(f"[{dt.datetime.now().isoformat()}] Vacancy info fetched!")
         # Performing update
-        db.update_vacancies(connection, fetched)
-        #db.delete_vacancies(connection, to_delete)
+        db.update_vacancies(db_con, fetched)
+        time.sleep(db_req_interval) # Letting database rest a little
+        time.sleep(get_random(web_req_interval[0], web_req_interval[1]))
